a9c0ea3c0d4f95650547b9c7c313c89d2118d379
Piero Toffanin <pt@masseranolabs.com>
2026-04-15 14:23:04 -0400

    Remove obsolete param

--- config.js
+++ config.js
@@ -51,7 +51,6 @@ Options:
 	--s3_access_key <key>	S3 access key, required if --s3_endpoint is set. (default: none)
 	--s3_force_path_style  Whether to force path style URLs for S3 objects. (default: false)
 	--s3_secret_key <secret>	S3 secret key, required if --s3_endpoint is set. (default: none) 
-	--s3_signature_version <version>	S3 signature version. (default: 4)
 	--s3_acl <canned-acl> S3 object acl. Can specify "none" to skip. (default: public-read)
 	--s3_upload_everything	Upload all task results to S3. (default: upload only all.zip archive)
 	--s3_ignore_ssl Whether to ignore SSL errors while connecting to S3. (default: false)
@@ -136,7 +135,6 @@ config.s3Bucket = argv.s3_bucket || fromConfigFile("s3Bucket", "");
 config.s3ForcePathStyle = argv.s3_force_path_style || fromConfigFile("s3ForcePathStyle", false);
 config.s3AccessKey = argv.s3_access_key || fromConfigFile("s3AccessKey", process.env.AWS_ACCESS_KEY_ID || "")
 config.s3SecretKey = argv.s3_secret_key || fromConfigFile("s3SecretKey", process.env.AWS_SECRET_ACCESS_KEY || "")
-config.s3SignatureVersion = argv.s3_signature_version || fromConfigFile("s3SignatureVersion", "4")
 config.s3ACL = argv.s3_acl || fromConfigFile("s3_acl", "public-read")
 config.s3UploadEverything = argv.s3_upload_everything || fromConfigFile("s3UploadEverything", false);
 config.s3IgnoreSSL = argv.s3_ignore_ssl || fromConfigFile("s3IgnoreSSL", false);
